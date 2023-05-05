d0160c1dd4b528337debadae9f5e25390b27ba5c
Cherch <b.cherchiglia@gmail.com>
2023-05-05 01:59:02 -0400

    Pseudocode Pt 2

--- script.js
+++ script.js
@@ -1,32 +1,39 @@
 ///// WELCOME to the First Chapter of Cherch Games: BOOMTOWN (A Letterkenny Themed Air Hockey Experience) /////
 
+
+
+
 //////////////////////////
 /*----- CONSTANTS -----*/
 
-// First, let's assign COLORS to PLAYER VALUE of 1 and -1 for our STICKS and TRANSPARENT for null
+// First, we'll assign COLORS to PLAYER VALUE of 1 and -1 for our STICKS and TRANSPARENT for null
 const playerColor = {
-    '1' : rgba(245, 245, 173, 0.86),
-    '1' : rgba(0,0,0, 0.86),
-    'null' : transparent,
+    '1': rgba(245, 245, 173, 0.86),
+    '1': rgba(0, 0, 0, 0.86),
+    'null': transparent,
 }
 
 
 ////////////////////////////////////////////////
 /*----- app's STATE (Variables) aka lets -----*/
 
+// Well, its hockey. So, we'll need a game board (the ICE), a scoreboard that updates at each goal per player, a winner, and a score (tally) for each player
+
 let board, score, goal, winner;
+let playerOneScore;
+
 
 
 /////////////////////////////////////////
 /*----- cached ELEMENT references -----*/
 
-// Next, we'll display a message in h2 delcaring who's TURN IT IS or WHO THE WINNER is //
+// Next, we'll select H2 to be the theatre that displays a message delcaring who's TURN IT IS or WHO THE WINNER is //
 const message = document.querySelector('h2');
 
-// Then, we'll activate our FACE OFF button at the top
+// Then, we'll declare our FACE OFF button at the top
 const faceOffBtn = document.querySelector('faceOffBtn')
 
-// Then we'll activate our PLAY AGAIN button at the bottom
+// Then we'll declare our PLAY AGAIN button at the bottom
 const playAgainBtn = document.querySelector('playAgainButton')
 
 
@@ -36,12 +43,39 @@ const playAgainBtn = document.querySelector('playAgainButton')
 
 // Next, we add an Event Listener to the BOARD 
 document.getElementById('ice').addEventListener('click', handleMove);
+
+// Next, we add an Event Listener to activate out FACE OFF 
+faceOffBtn.addEventListener('click', initialize);
+
+// Then we add an Event Listener to activate our PLAY AGAIN BUTTON
 playAgainBtn.addEventListener('click', initialize);
 
 
 
 /////////////////////////
 /*----- FUNCTIONS -----*/
+
+// First we need to activate our borders so the puck bounces off the BORDER of the ICE <div>
+
+// Then, We need to emulate PUCK movement and determine a max speed //
+
+// Next, we need to implement STICK contact with the PUCK in both Velocity and Intertia //
+
+// Then we need to make the PLAYER 1 STICK controllable using the Keyboard Arrows //
+
+// Then we need to assign a max and min speed for the directional toggle of PLAYER 1's STICK CONTROLS
+
+// Then, we need to activate the GOAL <divs> to emulate nets which means they must
+//  - collect a goal
+//  - add that goal to the player's SCORE
+//  - return the value of the GOAL SCORED to the SCOREBOARD
+//  - display the value of the SCOREBOARD
+
+// Then, we need to set borders to keep each Player's STICKS from crossing center ice
+
+
+//// I want to make my mouse cursor the PLAYER ONE STICK but it's also 3am and I'd love to ask you guys how to modify a cursor for the purpose of game design ////
+
 
 
 // Step ONE -- Init all STATE VARIABLES then call RENDER //
@@ -80,34 +114,38 @@ function render() {
 }
 
 
-/// RENDER THE BOARD  /////
+/// RENDER THE BOARD aka the ICE RINK!  /////
 
 
 
 ///// RENDER SCORE /////
-function renderScore () {
+function renderScore() {
     if (score >= '1') {
 
     } else if (score <= '-1') {
 
     } else {
-        
+
     }
 }
 
 
+////// RENDER THE SCOREBOARD /////
+
+
+
 
 ////// WINNING MESSAGE & TURN MESSAGE //////
-function renderMessage () {
+function renderMessage() {
     if (winner === 'T') {
-        
+
     }
     else if (winner >= '1') {
 
     }
     else if (winner <= '-1') {
 
-    } 
+    }
     else {
 
     }
